ea8819653f236af21c0ae121d80b6453f6295839
Ka (Ka Wai) Lau <kwlau107@gmail.com>
2024-01-28 23:49:50 +0000

    Finish primes

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
-    list = []
-    return list
+    lst = []
+    current_number = 2
+    while len(lst) < number_of_primes:
+        is_prime = True
+        for prime in lst:
+            if current_number % prime == 0:
+                is_prime = False
+        if is_prime:
+            lst.append(current_number)
+
+        current_number += 1
+
+    return lst
+
+
+print(primes(12))
